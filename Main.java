dedb8425359d9750d0ce43610695ede8496b7e1b
SimeonC2026 <simeon.chew2026@gbwl.org>
2025-12-09 16:05:16 +0000

    3.3 Notes

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+
+      // Create a new String -> SHORTCUT!
+      String st = "Hello World";
+      // But Strings are actually OBJECTS
+      // The original way to create a String:
+      String steph = new String("HELLO WORLD");
+
+      // All objects can be created using this pattern:
+      // ClassName objectName = new ClassName();
+      BuildABear puppy = new BuildABear("dog");
+
    }
 }
