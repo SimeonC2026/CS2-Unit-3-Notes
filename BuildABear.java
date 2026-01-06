f9cf63b23551e375874b4c57b0af85e06a944e3b
SimeonC2026 <simeon.chew2026@gbwl.org>
2026-01-06 16:06:25 +0000

    3.5 Notes

--- BuildABear.java
+++ BuildABear.java
@@ -37,4 +37,25 @@ public class BuildABear {
 
     // 3. METHODS (behaviors/actions)
 
+    // ACCESSORS ("getters") return the value of an instance var.
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFillLevel() {
+        return fillLevel;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+
+
+
+
 }
